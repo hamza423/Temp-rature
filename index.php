f77920eee21a3e31aef8003f985f19f7e2f6df07
Hamza Serhane <hamza42700@gmail.com>
2018-11-30 14:56:25 +0100

    modification

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-<?php
-echo 'hello world';
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <link type="text/css" rel="stylesheet" href="main.css">
+</head>
+
+    <body>
+    <h1>TEMP-RATURE</h1>
+        <img src="img/600_cours_congruences4.jpg">
+        <?php
+            echo 'hello world'; ?>
+    </body>
